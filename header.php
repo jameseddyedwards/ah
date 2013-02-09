a10f359b030c9654ee6b2931416fbf4816852a26
James 'Eddy' Edwards <jamesr.edwards@sky.com>
2013-02-09 01:01:21 +0000

    Add support for ie specific styling

--- header.php
+++ header.php
@@ -11,13 +11,16 @@
 ?>
 <!DOCTYPE html>
 <!--[if IE 6]>
-	<html id="ie6" <?php language_attributes(); ?>>
+	<html class="ie ie6 ltie9" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 7]>
-	<html id="ie7" <?php language_attributes(); ?>>
+	<html class="ie ie7 ltie9" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if IE 8]>
-	<html id="ie8" <?php language_attributes(); ?>>
+	<html class="ie ie8 ltie9" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 9]>
+	<html class="ie ie9" <?php language_attributes(); ?>>
 <![endif]-->
 <!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 	<html <?php language_attributes(); ?>>
